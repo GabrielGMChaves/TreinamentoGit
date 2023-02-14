14a9c472bdd3ca52d645a801d5ec38bfd9300fdd
Vitória Marques <101194572+ViFMarques@users.noreply.github.com>
2023-02-14 14:10:12 -0300

    primeiro commit

--- Test.js
+++ Test.js
@@ -1,3 +1,10 @@
-// O desafio é fazer uma página simples, 
-// usando html e js pra poder aparecer na tela a opção da pessoa escrever um texto pelo javascript 
-// e ele aparecer bem grande na tela no html
+const txt1 = document.getElementById('suaFrase');
+const btn1 = document.getElementById('btn1');
+const out1 = document.getElementById('output1');
+
+function aparecerTexto() {
+
+    out1.innerHTML = txt1.value;
+}
+
+btn1.addEventListener('click', aparecerTexto);
