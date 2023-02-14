35feaf8b0e049e7e63a75268b675fe13061f9226
Gabriel Miranda Chaves <bielzimchaves@gmail.com>
2023-02-14 11:41:24 -0300

    Treinamento

---
+++ Test.js
@@ -0,0 +1,3 @@
+// O desafio é fazer uma página simples, 
+// usando html e js pra poder aparecer na tela a opção da pessoa escrever um texto pelo javascript 
+// e ele aparecer bem grande na tela no html
